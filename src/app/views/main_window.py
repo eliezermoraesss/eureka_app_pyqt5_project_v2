49162d6da72a8ae49a1979d2754dca3cbc728a04
Eliezer Moraes Silva <eliezer.moraes@outlook.com>
2024-10-07 08:34:09 -0300

    feat(authorization): add 'Comercial' role for dashboard access. EUREKA

--- src/app/views/main_window.py
+++ src/app/views/main_window.py
@@ -38,7 +38,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setup_connections()
 
     def setup_connections(self):
-        @authorize(['admin', 'manager', 'Diretoria'], self)
+        @authorize(['admin', 'manager', 'Diretoria', 'Comercial'], self)
         def execute_dashboard_model(checked=False):
             process = QProcess()
             script_path = os.path.abspath(os.path.join(self.base_dir, '..', '..', 'models', 'dashboard_model.pyw'))
